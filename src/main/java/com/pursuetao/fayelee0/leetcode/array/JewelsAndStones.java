6bdd6b2bd7a9c44021e365950a94d4c4c8e160d5
Fei Li <wizard@pursuetao.com>
2020-01-05 16:19:22 +0800

    fix: add leetcode#771 uri

--- src/main/java/com/pursuetao/fayelee0/leetcode/array/JewelsAndStones.java
+++ src/main/java/com/pursuetao/fayelee0/leetcode/array/JewelsAndStones.java
@@ -4,7 +4,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
- * <a href="">LeetCode 771 Jewels and Stones (Easy)</a>
+ * <a href="https://leetcode.com/problems/jewels-and-stones">LeetCode 771 Jewels and Stones (Easy)</a>
  *
  * You're given strings J representing the types of stones that are jewels, and S representing the stones you have.  Each character in S is a type of stone you have.  You want to know how many of the stones you have are also jewels.
  *
