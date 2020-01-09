13eddb11d0c7a8f92270eccd5a0a69a12bf8c138
Fei Li <wizard@pursuetao.com>
2020-01-09 23:57:17 +0800

    fix: add leetcode#125 method

--- src/main/java/com/pursuetao/fayelee0/leetcode/array/Code125.java
+++ src/main/java/com/pursuetao/fayelee0/leetcode/array/Code125.java
@@ -43,4 +43,14 @@ public class Code125 {
         }
         return true;
     }
+
+    public static boolean isPalindrome2(String s) {
+        StringBuilder sb = new StringBuilder();
+        for (char c : s.toCharArray()) {
+            if (Character.isLetterOrDigit(c)) {
+                sb.append(Character.toLowerCase(c));
+            }
+        }
+        return sb.toString().equals(sb.reverse().toString());
+    }
 }
--- src/test/java/com/pursuetao/fayelee0/leetcode/array/TestCode125.java
+++ src/test/java/com/pursuetao/fayelee0/leetcode/array/TestCode125.java
@@ -11,4 +11,10 @@ public class TestCode125 {
         assertThat(Code125.isPalindrome("A man, a plan, a canal: Panama"), is(true));
         assertThat(Code125.isPalindrome("race a car"), is(false));
     }
+
+    @Test
+    public void isPalindrome2() {
+        assertThat(Code125.isPalindrome2("A man, a plan, a canal: Panama"), is(true));
+        assertThat(Code125.isPalindrome2("race a car"), is(false));
+    }
 }
