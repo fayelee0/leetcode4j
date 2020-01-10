264c80e5734d8b5bd3cf5be43c99e4f83b5b1bf2
Fei Li <wizard@pursuetao.com>
2020-01-10 23:37:18 +0800

    feat: leetcode#190 reverse bits

---
+++ src/test/java/com/pursuetao/fayelee0/leetcode/array/TestCode190.java
@@ -0,0 +1,14 @@
+package com.pursuetao.fayelee0.leetcode.array;
+
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class TestCode190 {
+    @Test
+    public void reverseBits() {
+        assertThat(Code190.reverseBits(0b00000010100101000001111010011100), is(0b00111001011110000010100101000000));
+        assertThat(Code190.reverseBits(0b11111111111111111111111111111101), is(0b10111111111111111111111111111111));
+    }
+}
