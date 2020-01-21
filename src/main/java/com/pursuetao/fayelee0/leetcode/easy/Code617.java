77690d4b78c22783a7a4b096c3665e8b61875587
Fei Li <wizard@pursuetao.com>
2020-01-21 23:00:07 +0800

    fix: update leetcode#617

--- src/main/java/com/pursuetao/fayelee0/leetcode/easy/Code617.java
+++ src/main/java/com/pursuetao/fayelee0/leetcode/easy/Code617.java
@@ -17,4 +17,17 @@ public class Code617 {
             return node;
         }
     }
+
+    public static TreeNode mergeTrees2(TreeNode t1, TreeNode t2) {
+        if (t1 == null) {
+            return t2;
+        } else if (t2 == null) {
+            return t1;
+        } else {
+            t1.val += t2.val;
+            t1.left = mergeTrees(t1.left, t2.left);
+            t1.right = mergeTrees(t1.right, t2.right);
+            return t1;
+        }
+    }
 }
