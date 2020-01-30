acbe0c9de0258fd04834e3ae0eb2d44eab427305
Fei Li <wizard@pursuetao.com>
2020-01-30 11:31:42 +0800

    feat leetcode#2 recursive implementation

--- src/main/java/com/pursuetao/fayelee0/leetcode/medium/Code2.java
+++ src/main/java/com/pursuetao/fayelee0/leetcode/medium/Code2.java
@@ -56,4 +56,63 @@ public class Code2 {
 
         return p;
     }
+
+    public static ListNode addTwoNumbers2(ListNode l1, ListNode l2) {
+        int c = 0;
+        int s = 0;
+        ListNode ret = new ListNode(0);
+        ListNode ptr = ret;
+        while (l1 != null || l2 != null) {
+            s = c;
+            if (l1 != null) {
+                s += l1.val;
+                l1 = l1.next;
+            }
+            if (l2 != null) {
+                s += l2.val;
+                l2 = l2.next;
+            }
+            if (s > 9) {
+                ptr.next = new ListNode(s - 10);
+                c = 1;
+            } else {
+                ptr.next = new ListNode(s);
+                c = 0;
+            }
+            ptr = ptr.next;
+        }
+        if (c != 0) {
+            ptr.next = new ListNode(c);
+        }
+        return ret.next;
+    }
+
+    public static ListNode addTwoNumbers3(ListNode l1, ListNode l2) {
+        return addTwoNumbers33(l1, l2, 0);
+    }
+
+    private static ListNode addTwoNumbers33(ListNode l1, ListNode l2, int carry) {
+        if (l1 == null && l2 == null) {
+            return (carry == 0) ? null : new ListNode(carry);
+        }
+
+        if (l1 == null) {
+            int s = l2.val + carry;
+            ListNode ret = new ListNode(s % 10);
+            ret.next = addTwoNumbers33(null, l2.next, s / 10);
+            return ret;
+        }
+
+        if (l2 == null) {
+            int s = l1.val + carry;
+            ListNode ret = new ListNode(s % 10);
+            ret.next = addTwoNumbers33(l1.next, null, s / 10);
+            return ret;
+        }
+
+        int s = l1.val + l2.val + carry;
+        ListNode ret = new ListNode(s % 10);
+        ret.next = addTwoNumbers33(l1.next, l2.next, s / 10);
+        return ret;
+    }
 }
